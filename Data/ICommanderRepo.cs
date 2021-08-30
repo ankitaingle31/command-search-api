6eed557341686c0fcafdfee62da9a8e44503fd6b
Ankita Ingle <77956627+ankitaingle31@users.noreply.github.com>
2021-08-30 09:03:03 -0400

    Add files via upload

---
+++ Data/ICommanderRepo.cs
@@ -0,0 +1,18 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public interface ICommanderRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Command> GetAllCommands(); // get all commands
+        Command GetCommandById(int id); // get a specific command
+        void CreateCommand(Command cmd); // create a new command
+        void UpdateCommand(Command cmd); // update an existing command
+        void DeleteCommand(Command cmd); // delete a command
+    }
+}
---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,23 @@
+﻿using Commander.Dtos;
+using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+            CreateMap<Command, CommandUpdateDto>(); // PUT           
+        }
+
+    }
+}
